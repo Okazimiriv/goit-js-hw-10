5dee5adca5c70542eb138f432c4265ead4e4d581
Oksana Komarnytska <okazimiriv@gmail.com>
2023-04-19 01:18:28 +0200

    step-1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+// const searchParams = new URLSearchParams({
+//   fields: 'name,capital,population,flags,languages',
+// });
+
+function fetchCountries(name) {
+  return fetch(
+    `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+
+fetchCountries('ukraine')
+  .then(name => console.log(name))
+  .catch(err => console.log(err));
