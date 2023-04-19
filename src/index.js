e1e344b609a93626fac88553d3264b395c2f2fef
Oksana Komarnytska <okazimiriv@gmail.com>
2023-04-19 18:21:41 +0200

    first marcup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
-const BASE_URL = 'https://restcountries.com/v3.1/name/';
-// const searchParams = new URLSearchParams({
-//   fields: 'name,capital,population,flags,languages',
-// });
+const searchCountries = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-function fetchCountries(name) {
-  return fetch(
-    `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
+searchCountries.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(evt) {
+  const value = searchCountries.value.trim();
+
+  fetchCountries(value)
+    .then(name => (countryList.innerHTML = createMarkup(name)))
+    .catch(err => console.log(err));
 }
 
-fetchCountries('ukraine')
-  .then(name => console.log(name))
-  .catch(err => console.log(err));
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ name: { official }, flags: { svg, alt } }) => `<li>
+      <img src="${svg}" alt="${alt}" width="50px">
+      <p>${official}</p>
+  </li>`
+    )
+    .join('');
+}
