5b4cd39183c0f0d5399932bd3ee6983e388f88a9
Okazimiriv <118137941+Okazimiriv@users.noreply.github.com>
2023-04-19 16:22:09 +0000

    Deploying to gh-pages from @ Okazimiriv/goit-js-hw-10@e1e344b609a93626fac88553d3264b395c2f2fef 🚀

--- index.3549dee4.js
+++
@@ -1,2 +0,0 @@
-var e;(e="ukraine",fetch(`https://restcountries.com/v3.1/name/${e}?fields=name,capital,population,flags,languages`).then((e=>{if(!e.ok)throw new Error(e.statusText);return e.json()}))).then((e=>console.log(e))).catch((e=>console.log(e)));
-//# sourceMappingURL=index.3549dee4.js.map
--- index.49122a1b.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;(n="ukraine",fetch("".concat("https://restcountries.com/v3.1/name/").concat(n,"?fields=name,capital,population,flags,languages")).then((function(n){if(!n.ok)throw new Error(n.statusText);return n.json()}))).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}))}();
-//# sourceMappingURL=index.49122a1b.js.map
